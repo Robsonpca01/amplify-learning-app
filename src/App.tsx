957bb20f28136ab51f7c12eb7c7e00be2cfcc6b3
Robson Carvalho <robson.santana_infobase@prestador.globo>
2021-09-14 07:21:20 -0300

    Added basic graphql api

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import Amplify from 'aws-amplify'
+import Amplify, {API, graphqlOperation } from 'aws-amplify'
 import { AmplifySignOut, withAuthenticator } from "@aws-amplify/ui-react"
 import awsconfig from './aws-exports';
+import { listTodos } from './graphql/queries';
 
 Amplify.configure(awsconfig);
 
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetchTodos();
+}, []);
+
+
+  const fetchTodos = async () => {
+    try {
+        const todosData: any = await API.graphql(graphqlOperation(listTodos));
+       const todoList = todosData.data.listTodos.items;
+        console.log('song list', todoList);
+        setTodos(todoList); 
+    } catch (error) {
+        console.log('error on fetching songs', error);
+    }
+};
+
+
+
   return (
     <div className="App">
       <header className="App-header">
